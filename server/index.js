c215354b5dc63ba468d20b18b76709a82ccfd09a
Janie Kim <janiekim@Janies-MacBook-Pro.local>
2020-10-26 14:20:35 -0500

    saving point

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const getProducts = require('getProducts')
+
+const app = express();
+const port = 3012;
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`)
+});
+
+app.get('/api/products', getProducts)
